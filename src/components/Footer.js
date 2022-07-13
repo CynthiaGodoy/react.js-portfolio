1d6d56268ff8dfcfb6fc35745137646dcdca0e80
Cynthia Godoy <cynthia@ck-interiors.com>
2022-07-12 21:22:25 -0600

    Create Footer, FooterStyle

---
+++ src/components/Footer.js
@@ -0,0 +1,36 @@
+import "./FooterStyles.css"
+
+import React from "react"
+
+import { FaHome, FaPhone, FaMailBulk, FaLinkedin, FaGithub } from "react-icons/fa";
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <div className="footer-container">
+                <div className="contactInfo">
+                <h4>Get in Touch!</h4>
+                    <div className="location">
+                        <FaHome size={18} style={{ color: "white", marginRight: "1rem" }} /><p>Denver, Colorado</p>
+                    </div>
+                    <div className="phone">
+                        <FaPhone size={18} style={{ color: "white", marginRight: "1rem" }} /><p>(720) 454-5143</p>
+                    </div>
+                    <div className="email">
+                        <FaMailBulk size={18} style={{ color: "white", marginRight: "1rem" }} /><p>Cynthia@ck-interiors.com</p>
+                    </div>
+                </div>
+                <div className="social">
+                    <h4>Social Media | More About Me</h4>
+                    <p>From Interior Designer to Software Developer. Educated in full-stack development from the University of Denver. </p>
+                        <div className="socialIcon">
+                            <FaLinkedin size={36} style={{ color: "white", marginRight: ".75rem" }} />
+                            <FaGithub size={36} style={{ color: "white", marginRight: ".75rem" }} />
+                        </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,26 +8,26 @@ import { FaBars, FaTimes } from "react-icons/fa";
 const Navbar = () => {
     const [click, btnClick] = useState(false);
     const handleClick = () => btnClick(!click);
-  return (
-    <div className="header">
-        <Link to="/">
-            <h1>Cynthia<span class="highlight">Godoy</span></h1>
-        </Link> 
-        <ul className={click ? "navbar active" : "navbar"}>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/portfolio">Portfolio</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
-        </ul>
-        <div className="hamburger" onClick={handleClick}>
-            {click ? (
-            <FaTimes size={20} style={{ color: "#333" }} />
-            ) : (
-            <FaBars size={20} style={{ color: "#333" }} />
-            )}
+        return (
+        <div className="header">
+            <Link to="/">
+                <h1>Cynthia<span class="highlight">Godoy</span></h1>
+            </Link> 
+            <ul className={click ? "navbar active" : "navbar"}>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/portfolio">Portfolio</Link></li>
+                <li><Link to="/contact">Contact</Link></li>
+            </ul>
+            <div className="hamburger" onClick={handleClick}>
+                {click ? (
+                <FaTimes size={20} style={{ color: "#333" }} />
+                ) : (
+                <FaBars size={20} style={{ color: "#333" }} />
+                )}
+            </div>
         </div>
-    </div>
-  );
+    );
 };
 
 export default Navbar;
