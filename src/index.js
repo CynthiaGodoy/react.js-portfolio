750013a43faf426459f78d5f97f49bc589c92164
Cynthia Godoy <cynthia@ck-interiors.com>
2022-07-19 13:33:19 -0600

    revise router

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 // import { BrowserRouter } from "react-router-dom"
-import { HashRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom"
 
 //THIS WORKED ON LOCAL HOST BUT NOT GH-PAGES THIS CREATED A WHITE SCREEN
 // const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -21,7 +21,7 @@ root.render(
 </HashRouter>
 );
 
-//THIS DIDN'T WORK AND CREATED A WHITESCREEN
+//THIS DIDN'T WORK AND CREATED A WHITE SCREEN
 // ReactDOM.render(
 //   <React.StrictMode>
 //     <HashRouter>
