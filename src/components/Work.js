6a2e213144d975bc6d3908117b43b9603a61e8fd
Cynthia Godoy <cynthia@ck-interiors.com>
2022-11-22 14:40:24 -0700

    push older version before dependabot

--- src/components/Work.js
+++ src/components/Work.js
@@ -4,19 +4,22 @@ import React from 'react';
 
 const Work = (props) => {
     return (
-        <div className="frontend-card img-hover-zoom--zoom-n-rotate">
+        <div className="fix-overlay">
+            <div className="frontend-card img-hover-zoom--zoom-n-rotate">
 
-            <a href={props.liveURL} target="_blank" rel="noreferrer">
-                <img src={props.imgsrc} className="background-frontend" alt={props.alt}/>
-            </a>
+                <a href={props.liveURL} target="_blank" rel="noreferrer">
+                    <img src={props.imgsrc} className="background-frontend" alt={props.alt}/>
+                </a>
 
-            <div className="overlay-frontend hide">
-                <h2 className="frontend-title">{props.title}</h2>
-                <p className="frontend-description">{props.description}</p>
-            </div>
+                <div className="overlay-frontend hide">
+                    <h2 className="frontend-title">{props.title}</h2>
+                    <p className="frontend-description">{props.description}</p>
+                </div>
 
-            <div className="frontend-btns">
-                <a href={props.github} target="_blank" rel="noreferrer" className="btn hvr-rectangle-out">GitHub</a>
+                <div className="frontend-btns">
+                    <a href={props.github} target="_blank" rel="noreferrer" className="btn hvr-rectangle-out">GitHub</a>
+                </div>
+
             </div>
         </div>
     )
