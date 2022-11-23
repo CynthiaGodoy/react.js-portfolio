b29a402bd777cb6a909f2002b0c520a25cdfbb5a
Cynthia Godoy <cynthia@ck-interiors.com>
2022-11-23 12:41:07 -0700

    fix hover css on portfolio

--- src/components/Work.js
+++ src/components/Work.js
@@ -3,23 +3,22 @@ import "./WorkGridStyles.css";
 import React from 'react';
 
 const Work = (props) => {
-    return (
-        <div className="fix-overlay">
+    return ( 
+        <div className="combine">
+            
             <div className="frontend-card img-hover-zoom--zoom-n-rotate">
-
                 <a href={props.liveURL} target="_blank" rel="noreferrer">
                     <img src={props.imgsrc} className="background-frontend" alt={props.alt}/>
                 </a>
 
-                <div className="overlay-frontend hide">
-                    <h2 className="frontend-title">{props.title}</h2>
-                    <p className="frontend-description">{props.description}</p>
-                </div>
+                    <div className="overlay-frontend hide">
+                        <h2 className="frontend-title">{props.title} </h2>
+                        <p className="frontend-description">{props.description}</p>
+                    </div>
 
-                <div className="frontend-btns">
-                    <a href={props.github} target="_blank" rel="noreferrer" className="btn hvr-rectangle-out">GitHub</a>
-                </div>
-
+                    <div className="frontend-btns">
+                        <a href={props.github} target="_blank" rel="noreferrer" className="btn hvr-rectangle-out">GitHub</a>
+                    </div>
             </div>
         </div>
     )
